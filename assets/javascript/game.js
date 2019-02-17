b44d6907358e308e68e3817c77e90adce43f33d4
Vince216 <vinceshury@gmail.com>
2019-02-17 15:41:22 -0500

    click events linked to crystals, game logic needs finalized

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 //          GLOBAL VARIABLES DECLARATIONS
 
-var computerScore, //computerScore = GENERATED EACH GAMEPLAY
-  userTotalScore = 0, //userTotalScore = THE SCORE ADDED UP BY CLICKING CRYSTALS
-  gamesLost = 0, //gamesLost = 0 + EACH GAME LOST
-  gamesWon = 0, //gamesWon = 0 + EACH GAME WON
-  crystalValue; //a variable to store each crystals value
+var computerScore = 0 , //computerScore = GENERATED EACH GAMEPLAY
+  userTotalScore = 0 , //userTotalScore = THE SCORE ADDED UP BY CLICKING CRYSTALS
+  gamesLost = 0 , //gamesLost = 0 + EACH GAME LOST
+  gamesWon = 0 , //gamesWon = 0 + EACH GAME WON
+  crystalValue = [] ; //an array to store the value of each crytal based on the index
 
 //          FUNCTION CREATION
 //Function to generate a random number, min , max parameters for either uses
@@ -14,17 +14,78 @@ function randomGen(min, max) {
 
 //picks a number between 19-120 for computer score
 function crytalNumbers() {
-  
+  for ( i=0 ; i < 4; i++ ){//FUNCTION TO GENERATE A NUMBER BETWEEN 1-12 FOR EACH CRYSTAL
+    crystalValue.push(randomGen(1,12));//loop to pick a number between 1-12 at random and assigns different values to each crystal.
+    console.log("assigned crytal value"+crystalValue.length);
+  }
 }
-//FUNCTION TO GENERATE A NUMBER BETWEEN 1-12 FOR EACH CRYSTAL
-//loop to
-//pick a number between 1-12 at random and assigns different values to each crystal.
+function computerScoreChoice() {
+  computerScore = randomGen(19,120);
+  console.log("assigned computer value");
+  $("#computer-number").text(computerScore);
+}
 
-//FUNCTION TO RESET GAME KEEPING SCORE
-//calls function to generate new random number 19-120
-//calls function to have each crystal assigned a new number between 1-12
-//sets user total score to zero
+function gameReset() {//FUNCTION TO RESET GAME KEEPING SCORE
+  computerScore = 0 ;//reset the computer score
+  userTotalScore = 0 ;//sets user total score to zero
+  crystalValue = [];//clears the crystal array
+  crytalNumbers();//calls function to have each crystal assigned a new number between 1-12
+  computerScoreChoice();//calls function to generate new random number 19-120
+  console.log("reset game")
 
+  console.log(computerScore + " as well as" + crystalValue[0] + crystalValue[1] + crystalValue[2] +crystalValue[3]);
+}
+
+
+  $(document).ready(function() {
+
+    gameReset();
+
+
+
+
+    $("#crystal-blue").on("click", function() {
+      userTotalScore += crystalValue[0]  ;
+      $("#current-score").text(userTotalScore);
+      console.log(crystalValue[0]);
+    });
+
+    $("#crystal-red").on("click", function() {
+      userTotalScore += crystalValue[1]  ;
+      $("#current-score").text(userTotalScore);
+      console.log(crystalValue[1]);
+    });
+
+    $("#crystal-gold").on("click", function() {
+      userTotalScore += crystalValue[2]  ;
+      $("#current-score").text(userTotalScore);
+      console.log(crystalValue[2]);
+    });
+
+    $("#crystal-green").on("click", function() {
+      userTotalScore += crystalValue[3]  ;
+      $("#current-score").text(userTotalScore);
+      console.log(crystalValue[3]);
+    });
+
+function gemClicks() {
+
+  if (userTotalScore > computerScore){
+      gamesLost++;
+      $("#loss").text("Losses: " + gamesLost);
+      gameReset();
+    }
+    else if ( userTotalScore === computerScore) {
+      gamesWon++;
+      $("#win").text("Wins: " + gamesWon);
+      gameReset();
+      }
+  }
+
+
+
+  
+  });
 //INSIDE DOCUMENT.READY FUNCTION BUILD GAME
 //on click even for each of the four gem images
 //when any of the four crytals is clicked
